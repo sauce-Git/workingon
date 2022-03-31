60eb9964cc5ada506a2faccf3b6b3a2dfd80da7a
sauce-Git <dadad1004@naver.com>
2022-03-31 11:40:35 +0100

    baek

--- c.cpp
+++ c.cpp
@@ -1,58 +1,27 @@
 #include <iostream>
 #include <queue>
-#define MAX 1000
+#define MAX 100000
 using namespace std;
 
-int arr[MAX][MAX];
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
-int n, m;
-queue<pair<int, int>> q;
-
-void print_arr();
+int arr[MAX * 2] = {0, };
+queue<int> q;
 
 int main(){
-	int x, y;
-	int nx, ny;
-	int count = 0;
-	cin >> m >> n;
-
-	for(int i = 0; i < n; i++)
-		for(int j = 0; j < m; j++){
-			cin >> arr[i][j];
-			if(arr[i][j] == 1) q.push(make_pair(i, j));
-		}
-
+	int n, k;
+	int max_dist = MAX;
+	cin >> n >> k;
+	q.push(n);
 	while(!q.empty()){
-		x = q.front().first;
-		y = q.front().second;
+		n = q.front();
 		q.pop();
-		for(int i = 0; i < 4; i++){
-			nx = x + dx[i];
-			ny = y + dy[i];
-			if(nx >= 0 && ny >= 0 && nx < n && ny < m && arr[nx][ny] == 0){
-				q.push(make_pair(nx, ny));
-				arr[nx][ny] = arr[x][y] + 1;
-				count = arr[nx][ny] - 1;
-			}
+		if(arr[n] < max_dist){
+			if(arr[n+1] == 0 || (arr[n+1] > arr[n] + 1)) arr[n+1] = arr[n] + 1;
+			if(arr[n-1] == 0 || (arr[n-1] > arr[n] + 1)) arr[n-1] = arr[n] + 1;
+			if(arr[2 * n] == 0 || (arr[2 * n] > arr[n] + 1)) arr[2 * n] = arr[n] + 1;
+
+			if(abs(k - (n+1)) < max_dist) max_dist = abs(k - (n+1));
+			if(abs(k - 2*n) < max_dist) max_dist = abs(k - 2*n);
 		}
 	}
-
-	for(int i = 0; i < n; i++)
-		for(int j = 0; j < m; j++)
-			if(!arr[i][j]) count = -1;
-
-	cout << count;
-
 	return 0;
-}
-
-void print_arr(){
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
-			cout << arr[i][j] << " ";
-		}
-		cout << "\n";
-	}
-			
 }
