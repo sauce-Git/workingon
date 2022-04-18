2843aa1f9395a5fc51784ef18d76aa86c56c15b2
sauce-Git <dadad1004@naver.com>
2022-04-18 05:32:10 +0100

    baek

--- c.cpp
+++ c.cpp
@@ -7,17 +7,17 @@ bool DP[2][MAX];
 
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
-	int max_num;
+	int max_num = 1;
 	bool is_empty[2];
 	string s;
 	cin >> s;
-	max_num = s.size();
 	DP[0][0]= 1;
 	for(int i = 1; i < s.size(); i++) {
 		DP[0][i] = 1;
 		if(s[i-1] == s[i]) {
 			DP[1][i] = 1;
 			is_empty[1] = false;
+			max_num = 2;
 		}
 	}
 
@@ -27,11 +27,15 @@ int main(){
 			if(DP[i%2][j] && (s[j-(int)ceil((float)i/2)] == s[j+i/2])){
 				DP[i%2][j] = 1;
 				is_empty[i%2] = false;
+			} else {
+				DP[i%2][j] = 0;
 			}
 		}
 		if(is_empty[0] & is_empty[1]) {
-			max_num = i - 1;
 			break;
+		}
+		if(!is_empty[i%2]){
+			max_num = i + 1;
 		}
 	}
 
